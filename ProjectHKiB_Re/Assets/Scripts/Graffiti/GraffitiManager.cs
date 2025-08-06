9428c1dfe11d9b13e0776b78ba03168b27109777
possiroom <178400765+possiroom@users.noreply.github.com>
2025-08-06 20:52:02 +0900

    250806

--- ProjectHKiB_Re/Assets/Scripts/Graffiti/GraffitiManager.cs
+++ ProjectHKiB_Re/Assets/Scripts/Graffiti/GraffitiManager.cs
@@ -1,22 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GraffitiManager : MonoBehaviour
 {
-    private List<Vector2> graffitiVectorSet = new List<Vector2>();
-    private List<PlayerSkillDataSO> skillList = new List<SkillDataS0>();
+    private List<Vector2> graffitiProgress = new();
+    private List<PlayerSkillDataSO> skillList = new();
     
-    int completedSkillNum = -1;
-
+    public PlayerSkillDataSO[] SkillListTemp;
+    private int completedSkillNum = -1;
+    private bool check = true;
     //��ų �߰��ڵ�
-
-    public void Start()
-    {
-        PlayerSkillDataSO Windmill = new PlayerSkillDataSO();
-        Windmill.graffitiCode={(0,0), (0,1),(1,0),(0,-1),(-1,0)};
-        skillList.Add(Windmill);
-    }
 
     // public bool ContainsVector(Vector2 target)
     // {
@@ -28,15 +23,13 @@ public class GraffitiManager : MonoBehaviour
 
     //���� ��ǥ currentVector
 
-
     /// <summary>
     /// called everytime when player moves in graffiti
     /// </summary>
     /// <param name="currentPlayerPos"> current player pos</param>
     public void CheckGraffitiProgress(Vector2 currentPlayerPos) //�̰� �� �ǵ�� ��¼�� �̰� �� ����
     {  
-        bool check = true;
-        bool end = false;
+        
         // ������ ������ ȣ��
 
         // ValidateVectorSet�� 
@@ -48,74 +41,92 @@ public class GraffitiManager : MonoBehaviour
         // CheckCompleted >= 0 �� ���� �ǵ��, completedSkillNum�� ��
         //get player's vector
         
-
+        graffitiProgress.Add(currentPlayerPos);
+        
+        //print Vectorset();
         int completedSkillNum = CheckCompleted();
 
-            graffitiVectorSet.Add(currentPlayerPos);
-            check = ValidateVectorSet(graffitiVectorSet);
-            //print Vectorset();
-        
-
-        if(completedSkillNum >= 0){
+        if(completedSkillNum >= 0)
+        {
             //success feedback-> green effect
-            CheckGraffitiComplete(completedSkillNum);
-            end=true;
+            EndGraffiti();
+            return;
         }
 
+
+        check = ValidateProgress(graffitiProgress);
         //fail
-        if(!check){
+        if (!check)
+        {
             //fail feedback-> red effect
-            end=true;
-        }
-
-        if (end) {
-            graffitiVectorSet.Clear();
+            graffitiProgress.Clear();
         }
     }
 
-    public void CheckGraffitiComplete(int num)
+    private void EndGraffiti()
     {
-        switch(num){
-            case 0: //windmill skill on
+        TriggerSkill(completedSkillNum);
+        graffitiProgress.Clear();
+    }
+
+    private bool ValidateProgress(List<Vector2> graffitiProgress)
+    {
+        for(int skill = 0; skill < SkillListTemp.Length; skill++)
+        {
+            for (int i = 0; i < SkillListTemp[skill].graffitiAllCases.Count; i++)
+            {
+                List<Vector2> graffitiCode = SkillListTemp[skill].graffitiAllCases[i].code;
+                bool isSkillValid = true;
+                for (int j = 0; j < graffitiProgress.Count; j++)
+                {
+                    if (!graffitiCode.Contains(graffitiProgress[j]))
+                    {
+                        isSkillValid = false;
+                        break;
+                    }
+                }
+                if (isSkillValid) return true;
+            }
         }
-        return num=-1;
+        return false;
+    }
+
+    public void TriggerSkill(int num)
+    {
+        // trigger skill
+        // trigger func from player script
     }
     // �׶���Ƽ ���� ���� ��ǲ�� ������ ȣ��
     // �÷��̾ ��ų �ߵ��ϵ��� ��
 
-    List<Vector2> VectorSet()
-    {
-        List<Vector2> patterns=null;
+    //List<Vector2> VectorSet()
+    //{
+    //    List<Vector2> patterns=null;
+//
+    //    foreach (var graffitiCode in SkillListTemp.graffitiAllCases){
+    //        if (graffitiProgress.All(vector => graffitiCode.code.Contains(vector)))
+    //            {
+    //                patterns = graffitiCode.code;  
+    //                break;
+    //            }
+    //    }
+    //    return patterns;
+    //}
 
-        foreach (var graffitiCode in Windmill.graffitiAllCases){
-            if (graffitiVectorSet.All(vector => graffitiCode.code.Contains(vector)))
-                {
-                    patterns = graffitiCode.code;  
-                    break;
-                }
-        }
-        return patterns;
-
-    }
-
-    bool ValidateVectorSet(Vector2 vector)
-    {
-        foreach (var graffitiCode in Windmill.graffitiAllCases){
-            if (graffitiVectorSet.All(Vector2 => Windmill.graffitiAllCases.Contains(Vector2)))
-                return true;
-        }
-        return false;
-
-    }
+    
 
     // �̵��� ������ vectorset�� ��ų �߿� ���ԵǴ��� Ȯ��
     int CheckCompleted()
     {
-        foreach (var graffitiCode in Windmill.graffitiAllCases)
-            if (graffitiCode.code.SequenceEqual(graffitiVectorSet))
-                return 0;
-
-
+        for(int skill = 0; skill < SkillListTemp.Length; skill++)
+        {
+            for (int i = 0; i < SkillListTemp[skill].graffitiAllCases.Count; i++)
+            {
+                List<Vector2> graffitiCode = SkillListTemp[skill].graffitiAllCases[i].code;
+                if (graffitiCode == graffitiProgress)
+                    return skill;
+            }
+        }
         return -1;
     }
     // ���� vectorset�� ��ų �� �ϳ��� ��ġ�ϴ��� Ȯ��
