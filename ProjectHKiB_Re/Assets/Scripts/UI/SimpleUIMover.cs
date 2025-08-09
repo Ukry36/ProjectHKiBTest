b0964afe5f1c0bde8890a2db1d1336e2af31a33d
Ukry36 <128790404+Ukry36@users.noreply.github.com>
2025-08-10 04:46:57 +0900

    20250809

--- ProjectHKiB_Re/Assets/Scripts/UI/SimpleUIMover.cs
+++ ProjectHKiB_Re/Assets/Scripts/UI/SimpleUIMover.cs
@@ -1,34 +1,72 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SimpleUIMover : MonoBehaviour
 {
-    [SerializeField] private Vector2 _shift;
+    public enum MoveMode
+    {
+        Move, Enable, Disable
+    }
+    [SerializeField] private Vector2[] _presetLocalPositions;
+    [SerializeField] private int _truePositionIndex;
+    [SerializeField] private int _falsePositionIndex;
     [SerializeField] private float _delay;
     [SerializeField] private float _duration;
     private Sequence _sequence;
-    public void MovePosition()
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _inAnimationDuration;
+    [SerializeField] private float _outAnimationDuration;
+
+    public UnityEvent<bool> OnSetInteractable;
+
+    public void SetInteractable(bool set)
     {
+        OnSetInteractable?.Invoke(set);
+    }
+
+    public void MovePosition(Vector2 localPos, MoveMode mode)
+    {
+
+        SetInteractable(false);
         Complete();
         _sequence = DOTween.Sequence();
         _sequence.AppendInterval(_delay);
-        _sequence.Append(transform.DOLocalMove(_shift, _duration));
+        if (_animator != null && (mode == MoveMode.Move || mode == MoveMode.Disable))
+        {
+            _sequence.AppendCallback(() => _animator.Play("Out"));
+            _sequence.AppendInterval(_inAnimationDuration);
+            _sequence.AppendCallback(() => _animator.Play(mode == MoveMode.Disable ? "Disabled" : "Moving"));
+        }
+        _sequence.Append(transform.DOLocalMove(localPos, _duration));
+        if (mode == MoveMode.Move || mode == MoveMode.Enable)
+        {
+            if (_animator != null)
+            {
+                _sequence.AppendCallback(() => _animator.Play("In"));
+                _sequence.AppendInterval(_outAnimationDuration);
+                _sequence.AppendCallback(() => _animator.Play("Enabled"));
+            }
+            _sequence.AppendCallback(() => SetInteractable(true));
+        }
         _sequence.Play();
     }
-
-    public void ResetPosition()
+    public void MovePosition(int index, MoveMode mode)
     {
-        Complete();
-        _sequence = DOTween.Sequence();
-        _sequence.AppendInterval(_delay);
-        _sequence.Append(transform.DOLocalMove(Vector2.zero, _duration));
-        _sequence.Play();
+        if (_presetLocalPositions.Length <= index) return;
+        MovePosition(_presetLocalPositions[index], mode);
     }
-
+    public void MovePosition() => MovePosition(0, MoveMode.Move);
+    public void MovePosition(int index) => MovePosition(index, MoveMode.Move);
+    public void ResetPosition() => MovePosition(Vector2.zero, MoveMode.Move);
+    public void Enable(Vector2 localPos) => MovePosition(localPos, MoveMode.Enable);
+    public void Enable() => Enable(Vector2.zero);
+    public void Disable() => MovePosition(transform.localPosition, MoveMode.Disable);
     public void TogglePosition(bool set)
     {
-        if (set) MovePosition();
-        else ResetPosition();
+        if (set) MovePosition(_truePositionIndex, MoveMode.Move);
+        else MovePosition(_falsePositionIndex, MoveMode.Move);
     }
 
     public void Kill() => _sequence?.Kill();
